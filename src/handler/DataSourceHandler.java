8588d9b021cee4f90bfe65dbacfc78300bae240b
Yannick CHAZE <yannick@wearecloud.com>
2008-12-08 12:25:53 +0100

    Updtae for test connection

--- src/handler/DataSourceHandler.java
+++ src/handler/DataSourceHandler.java
@@ -1,30 +1,47 @@
 package handler;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import merapi.messages.IMessage;
 import merapi.messages.IMessageHandler;
+import merapi.messages.Message;
+import merapi.Bridge;;
 
 public class DataSourceHandler implements IMessageHandler {
 	private DriverManagerDataSource ds = new DriverManagerDataSource();
 	private String st = new String();
+	
 	@Override
 	public void handleMessage(IMessage message) {
 		try{
 			st = (String)message.getData();
-			// PARSING
 			String[] values = st.split("##");
+			ds.setUsername(values[0]);
 			ds.setPassword(values[1]);
 			ds.setDriverClassName("oracle.jdbc.driver.OracleDriver");
 			ds.setUrl("jdbc:oracle:thin:@localhost:1521:xe");
-			ds.setUsername(values[0]);
-			
-			Connection con = ds.getConnection();
-			System.out.println(con.getMetaData());
+			testBase();
 		}catch(Exception e){
 			e.printStackTrace();
 		}
+	}
+	
+	private void testBase(){
+		Message m;
+		try {
+			Connection con = ds.getConnection();
+			m = new Message ("testBase",null,"Succes");
+		} catch (SQLException e) {
+			m = new Message ("testBase",null,e.getMessage());
+		}
+			try {
+				Bridge.getInstance().sendMessage(m);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 	}
 }
--- src/server/Server.java
+++ src/server/Server.java
@@ -2,9 +2,6 @@ package server;
 
 import handler.DataSourceHandler;
 
-import java.sql.Driver;
-import java.sql.DriverManager;
-import java.util.Enumeration;
 import java.util.logging.Logger;
 import merapi.Bridge;
 public class Server {
@@ -12,13 +9,6 @@ public class Server {
 	public static void main(String[] args) {
 		Logger.getAnonymousLogger().info("Starting Java Server for PeperBoy...");
 		Logger.getAnonymousLogger().info("Registrering a new data source handler");
-		for (Enumeration e = DriverManager.getDrivers(); e.hasMoreElements();){
-		    Driver driver = (Driver)e.nextElement();
-		    int majorVersion = driver.getMajorVersion();
-		    int minorVersion = driver.getMinorVersion();
-		    System.out.println("Driver = "+driver.getClass()+
-		        " v"+majorVersion+"."+minorVersion);
-		}
 		Bridge.getInstance().registerMessageHandler("dataSource", new DataSourceHandler());
 	}
 
