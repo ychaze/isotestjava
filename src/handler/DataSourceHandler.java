f559a76db59a1940eb60cdd2fca0c580f0fc8dea
Yannick CHAZE <yannick@wearecloud.com>
2008-12-10 16:37:07 +0100

    ADD request for databases

--- src/handler/DataSourceHandler.java
+++ src/handler/DataSourceHandler.java
@@ -1,7 +1,9 @@
 package handler;
 import java.sql.SQLException;
+import java.util.List;
 import java.util.logging.Logger;
 
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import util.DataSourceFactory;
 
@@ -19,8 +21,31 @@ public class DataSourceHandler implements IMessageHandler {
 			st = (String)message.getData();
 			String[] values = st.split("##");
 			ds = DataSourceFactory.createDS(values[3], values[0], values[1], Integer.decode(values[2]));
-			Bridge.getInstance().sendMessage(new Message("testBase",null,"Trying to connect the DB..."));
-			testBase();
+			System.out.println(message.getSourceType());
+			if(message.getSourceType() !=null){
+				JdbcTemplate t = new JdbcTemplate(ds);
+				if(message.getSourceType().compareTo("getDBSqlServer")==0){
+					if (testBase()){
+						List l = null;
+						l = t.queryForList("SELECT NAME FROM SYS.DATABASES");
+						Bridge.getInstance().sendMessage(new Message("DBSqlServer",null,l));
+					}
+				}
+				else if(message.getSourceType().compareTo("getDBMySql")==0){
+					if (testBase()){
+						List l = null;
+						l = t.queryForList("show databases");
+						Bridge.getInstance().sendMessage(new Message("DBMySql",null,l));
+					}
+				}
+			}
+			else{
+				Bridge.getInstance().sendMessage(new Message("testBase",null,"Trying to connect to the DB..."));
+				if(testBase()){
+					Bridge.getInstance().sendMessage(new Message ("testBase",null,"Succes"));
+				}
+			}
+			
 		}catch(Exception e){
 			Message m = new Message ("testBase",null,e.getMessage());
 			try {
@@ -36,20 +61,19 @@ public class DataSourceHandler implements IMessageHandler {
 		boolean b=true;
 		try {
 				ds.getConnection();
-				m = new Message ("testBase",null,"Succes");
 				synchronized(this){
 					notify();
 				}
 		} catch (SQLException e) {
 			m = new Message ("testBase",null,"ERROR : "+e.getMessage());
-			b=false;
-		}
 			try {
 				Bridge.getInstance().sendMessage(m);
 				Logger.getAnonymousLogger().info(m.getData().toString());
-			} catch (Exception e) {
-				e.printStackTrace();
+			} catch (Exception e2) {
+				e2.printStackTrace();
 			}
+			b=false;
+		}
 			return b;
 	}
 
